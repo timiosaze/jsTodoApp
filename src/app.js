832faf2b645aa52c26f4be3e1ec59e924c5a3d8f
timiosaze <timiade1993@gmail.com>
2024-10-21 16:35:31 +0100

    App Working Well

--- src/app.js
+++ src/app.js
@@ -1,4 +1,46 @@
 $('#button1').hide(); //hide all the reason text
-$('#firstTask').on('click', function() {
+$('.task').on('click', function() {
   $(this).children('#button1').toggle();
 });
+let tasks = [];
+let theTasks = document.getElementById('theTasks');
+
+//Fetch all tasks from the local Storage
+let storedTasks = JSON.parse(localStorage.getItem('tasks'));
+let all = ''; 
+for(let i =0; i<storedTasks.length; i++){
+    let eachTask =  '<li id="' + storedTasks[i] + '" class="task flex rounded-sm text-sm p-4 border border-solid border-black"><div class=""><button id="' + storedTasks[i] + '" onclick="deleteTask('+"'"+storedTasks[i]+"'"+')" class="mr-2 text-white bg-red-700 text-xs py-1 px-1 rounded-md">Delete</button></div>'+ storedTasks[i] +'</li>';
+    all += eachTask;
+}
+theTasks.innerHTML = all;
+
+//Add a Task to the Array and // Populate HTML
+function addTask(){
+    let task = document.getElementById('task');
+    console.log(task.value);
+    if(task.value.length > 0 && !tasks.includes(task.value)){
+        tasks.push(task.value);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        let storedTasks = JSON.parse(localStorage.getItem('tasks'));
+        let all = ''; 
+        for(let i =0; i<storedTasks.length; i++){
+            let eachTask =  '<li id="' + storedTasks[i] + '" class="task flex rounded-sm text-sm p-4 border border-solid border-black"><div class=""><button id="' + storedTasks[i] + '" onclick="deleteTask('+"'"+storedTasks[i]+"'"+')" class="mr-2 text-white bg-red-700 text-xs py-1 px-1 rounded-md">Delete</button></div>'+ storedTasks[i] +'</li>';
+            all += eachTask;
+        }
+        theTasks.innerHTML = all;   
+    }
+    document.getElementById('task').value = '';
+}
+
+//Delete a Task 
+function deleteTask(task){
+    let storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    let index = storedTasks.indexOf(task);
+    console.log(index);
+    storedTasks.splice(index, 1); 
+    localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    setTimeout(function(){
+        window.location.reload();
+    });
+  
+}
